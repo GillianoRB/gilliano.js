5d06806004dd6be67df172d06ed99be12a5ba1d1
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-10-28 19:34:17 -0300

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -4,7 +4,7 @@ exports.run = async (bot, msg, args) => {
     if(msg.author.id != `244169411026485259`) return msg.channel.send (`$(msg.author), you may now use this command!`)
       if(!args || args.size < 1) return msg.channel.send("Please provide a valid command");
       delete require.cache[require.resolve(`./$(args[0]).js`)];
-      msg.reply('The command ${args[0] has been reloaded!}')
+      msg.reply('The command ${args[0] has been reloaded!}');
 
 }
 
