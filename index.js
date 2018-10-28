a56c6b8140c997e6a81b86a8f24da9eb929ad80b
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-10-28 16:37:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -67,15 +67,7 @@ bot.on("message", async message => {
     message.delete();
   }
 	
-  if (message.content === "!sendguildmessages") {
-        var guildList = client.guilds.array();
-        try {
-            guildList.forEach(guild => guild.defaultChannel.send("general", "Global"));
-        } catch (err) {
-            console.log("Could not send message to " + guild.name);
-        }
-    }
-});
+  
 
 
   if (message.content.startsWith("hey")) {
