c25f85cf53b6f5514ef92a6cea336100c888e508
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-10-28 19:31:46 -0300

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,4 +1,6 @@
-module.exports.run = async (bot, msg, args) => {
+const Discord = require("discord.js");
+
+exports.run = async (bot, msg, args) => {
     if(msg.author.id != `244169411026485259`) return msg.channel.send (`$(msg.author), you may now use this command!`)
       if(!args || args.size < 1) return msg.channel.send("Please provide a valid command");
       delete require.cache[require.resolve(`./$(args[0]).js`)];
@@ -6,6 +8,7 @@ module.exports.run = async (bot, msg, args) => {
 
 }
 
-module.exports.help = {
+exports.help = {
     name = "reload"
+    category: 'OWNER BOT'
 }
