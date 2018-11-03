f619ec24f3eefde0d1d04e4e17ac216b008422bd
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-11-02 21:32:31 -0300

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -3,11 +3,12 @@ exports.run = async (bot, message, args) => {
     message.delete();
     
     const embed = new Discord.RichEmbed()
-    .setTitle(`${message.author.username} Said..`)
-    .setColor("RANDOM")
-    .setDescription(args.join(' '))
- 
-    let msg = await message.channel.send(embed)
+        .setColor("#ffffff") //To change color do .setcolor("#fffff")
+        .setFooter('^^^^^^^^^^')
+        .setDescription(args.join(' '))
+        .setTitle(`${message.author.username} Said`);
+
+        let msg = await message.channel.send(embed)
 }
 
 module.exports.help = {
