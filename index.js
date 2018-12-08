9dac5021e1596ce632d2ed1c8d2fc18f9d62b3c2
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-08 22:13:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on("ready", () => {
 
 bot.on('guildMemberAdd', member => {
     member.guild.channels.get('520778562421129219').send('**' + member.user.username + '**, has joined the server!'); 
-    member.addRole(member.guild.roles.find(role => role.name === "members"));
+    member.addRole(member.guild.roles.find(role => role.name === "Guests"));
 });
 
 bot.on('guildMemberRemove', member => {
