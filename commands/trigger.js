5f9a2f780d9ae97742d3b56273230f5d378ed883
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-07 23:14:55 -0300

    Add files via upload

--- commands/trigger.js
+++ commands/trigger.js
@@ -1,10 +1,10 @@
 module.exports.run = async (bot, message, args) => {
 
-    let profilepic = message.author.avatarURL;
+    let user = message.mentions.users.first() || message.author;
 
     message.channel.send({
         file: {
-            attachment: "https://cute-api.tk/v1/generate/triggered?url=" + profilepic,
+            attachment: "https://cute-api.tk/v1/generate/triggered?url=" + user.displayAvatarURL,
             name: "triggered.gif"
         }
     });
