f229c41db5b790cd4e53b783bacd61ebc945096b
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-08 02:17:48 +0000

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send({
         file: {
             attachment: "https://cute-api.tk/v1/generate/triggered?url=" + user.displayAvatarURL,
-            name: "triggered.gif"
+            name: "trigger.gif"
         }
     });
 
