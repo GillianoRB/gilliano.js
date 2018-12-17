c9c48bf37a149829eef7fdac60ca48f180134cad
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-17 01:00:07 +0000

    Update hypixelinfo.js

--- commands/hypixelinfo.js
+++ commands/hypixelinfo.js
@@ -4,8 +4,6 @@ exports.run = async (bot, message, args) => {
     
     let embed = new Discord.RichEmbed()
     .setColor("#4bf442")
-    .setFooter(`User ID: ${user.id}`)
-    .setTimestamp()
     .addField("**hypixel** is a minecraft server based on many fun minigames.", "Gamemodes")
     .addField("Hypixel includes games such as Skywars, Bedwars and Build Battle. Hypixel has a lot of different communities, such as PvP, Building and roleplaying Hypixel also has different ranks and cosmetics.", "Shop")
     .addField("You can buy the ranks on store.hypixel.net where you can buy many more things than ranks. https://i.imgur.com/EEG244D.png", "Thanks For Your Time");
