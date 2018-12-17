393b71b5ffda8d4f5b641a2daaab5c1fe6485a32
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-17 01:08:25 +0000

    Update hpachievements.js

--- commands/hpachievements.js
+++ commands/hpachievements.js
@@ -6,10 +6,8 @@ exports.run = async (bot, message, args) => {
     
     let embed = new Discord.RichEmbed()
     .setColor("#4bf442")
-    .setAuthor(`${user.tag}'s Info`, `${user.avatarURL}`)
-    .setFooter(`User ID: ${user.id}`)
     .setTimestamp()
-    .addField("Hypixel includes many achievements, there is achievements in EVERY gamemode except for PTL lobby games! Hypixel has AP or Achievement Points, where you gain Hypixel XP and cosmetics for completing achievements!");
+    .addField("Hypixel includes many achievements, there is achievements in EVERY gamemode except for PTL lobby games! Hypixel has AP or Achievement Points, where you gain Hypixel XP and cosmetics for completing achievements!", "Thanks For Reading");
 
     message.channel.send(embed)
     }
