e0c7d444d1b49336be76db733dcb901cc592a57f
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-17 00:53:55 +0000

    Update hypixelinfo.js

--- commands/hypixelinfo.js
+++ commands/hypixelinfo.js
@@ -9,8 +9,10 @@ exports.run = async (bot, message, args) => {
     .setAuthor(`${user.tag}'s Info`, `${user.avatarURL}`)
     .setFooter(`User ID: ${user.id}`)
     .setTimestamp()
-    .addField("**hypixel** is a minecraft server based on many fun minigames. Hypixel includes games such as Skywars, Bedwars and Build Battle. Hypixel has a lot of different communities, such as PvP, Building and roleplaying Hypixel also has different ranks and cosmetics. You can buy the ranks on store.hypixel.net where you can buy many more things than ranks. https://i.imgur.com/EEG244D.png");
-    
+    .addField("**hypixel** is a minecraft server based on many fun minigames.")
+    .addField("Hypixel includes games such as Skywars, Bedwars and Build Battle. Hypixel has a lot of different communities, such as PvP, Building and roleplaying Hypixel also has different ranks and cosmetics.")
+    .addField("You can buy the ranks on store.hypixel.net where you can buy many more things than ranks. https://i.imgur.com/EEG244D.png");
+
     message.channel.send(embed)
     }
     exports.help = {
