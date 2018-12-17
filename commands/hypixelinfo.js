3aa0407b62a90923bc567f9dff92cdb269d3b611
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2018-12-17 00:58:39 +0000

    Update hypixelinfo.js

--- commands/hypixelinfo.js
+++ commands/hypixelinfo.js
@@ -1,17 +1,14 @@
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, args) => {
-
-    let user = message.mentions.users.first() || message.author;
     
     let embed = new Discord.RichEmbed()
     .setColor("#4bf442")
-    .setAuthor(`${user.tag}'s Info`, `${user.avatarURL}`)
     .setFooter(`User ID: ${user.id}`)
     .setTimestamp()
-    .addField("**hypixel** is a minecraft server based on many fun minigames.")
-    .addField("Hypixel includes games such as Skywars, Bedwars and Build Battle. Hypixel has a lot of different communities, such as PvP, Building and roleplaying Hypixel also has different ranks and cosmetics.")
-    .addField("You can buy the ranks on store.hypixel.net where you can buy many more things than ranks. https://i.imgur.com/EEG244D.png");
+    .addField("**hypixel** is a minecraft server based on many fun minigames.", "Gamemodes")
+    .addField("Hypixel includes games such as Skywars, Bedwars and Build Battle. Hypixel has a lot of different communities, such as PvP, Building and roleplaying Hypixel also has different ranks and cosmetics.", "Shop")
+    .addField("You can buy the ranks on store.hypixel.net where you can buy many more things than ranks. https://i.imgur.com/EEG244D.png", "Thanks For Your Time");
 
     message.channel.send(embed)
     }
