382c80a789af16bf94bc7d84ca761e61842ab509
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-01-14 00:06:00 +0000

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,27 +1,28 @@
-exports.run = (guild, message, args) => {
-  let modRole = message.guild.roles.find("name", "Staff");
-  if(message.member.roles.has(modRole.id)) {
-      let reason = args.slice(1).join(' ');
-      let user = message.mentions.users.first();
-      let modlog = guild.channels.find('name', 'log');
+const Discord = require("discord.js");
 
-      if (!modlog) 
-          return message.reply('I cannot find a log channel');
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
 
-      if (reason.length < 1) 
-          return message.reply('You must supply a reason for the warning.');
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Warning")
+    .setColor("#15f153")
+    .addField("Warned User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Warned By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
 
-      if (message.mentions.users.size < 1) 
-          return message.reply('You must mention someone to warn them.').catch(console.error);
+    let reportschannel = message.guild.channels.find(`name`, "logs");
+    if(!reportschannel) return message.channel.send("Couldn't find logs channel.");
 
-      const embed = new Discord.RichEmbed()
-          .setColor(0x8cff00)
-          .setTimestamp()
-          .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
 
-          return message.channel.send({embed});
-  }
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
 }
-exports.help = {
+ 
+module.exports.help = {
   name: "warn"
 }
