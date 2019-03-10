98f07809bbfeb7975e12b27f09252aa958fd6ae2
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-03-10 14:18:38 -0300

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,14 +1,25 @@
 const Discord = require("discord.js");
 const ms = require("ms");
+const botconfig = require("../botconfig.json");
+const red = botconfig.red;
+const green = botconfig.green;
+const orange = botconfig.orange;
 
 module.exports.run = async (bot, message, args) => {
 
-  //!tempmute @user 1s/m/h/d
 
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No can do.");
+  if(args[0] == "help"){
+    message.reply("Usage: !tempmute <user> <1s/m/h/d>");
+    return;
+  }
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("ADMINISTRATION")) return message.reply("Can't mute them!");
-  let muterole = message.guild.roles.find(`name`, "Muted");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  let reason = args.slice(2).join(" ");
+  if(!reason) return message.reply("Please supply a reason.");
+
+  let muterole = message.guild.roles.find(`name`, "muted");
   //start of create role
   if(!muterole){
     try{
@@ -31,8 +42,28 @@ module.exports.run = async (bot, message, args) => {
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
 
+  message.delete().catch(O_o=>{});
+
+  try{
+    await tomute.send(`Hi! You've been muted for ${mutetime}. Sorry!`)
+  }catch(e){
+    message.channel.send(`A user has been muted... but their DMs are locked. They will be muted for ${mutetime}`)
+  }
+
+  let muteembed = new Discord.RichEmbed()
+  .setDescription(`Mute executed by ${message.author}`)
+  .setColor(orange)
+  .addField("Muted User", tomute)
+  .addField("Muted in", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Length", mutetime)
+  .addField("Reason", reason);
+
+  let incidentschannel = message.guild.channels.find(`name`, "logs");
+  if(!incidentschannel) return message.reply("Please create a log channel first! must type it correctly `logs`");
+  incidentschannel.send(muteembed);
+
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
@@ -45,4 +76,3 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: "tempmute"
-}
