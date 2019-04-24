f55ab6d17ba60816ecc28392f569b798ba9b99bd
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-24 16:09:12 -0300

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -2,7 +2,6 @@ exports.run = (client, message) => {
     var role = message.guild.roles.find(role => role.name === "Guests");
     message.member.addRole(role);
     message.author.send('Hello thank you for verifying! ');
-    message.delete{}
     }
   
 
