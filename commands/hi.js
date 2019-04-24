de8f93de286fc25a4f7fd0c164ac2570e93b699d
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-24 16:00:16 -0300

    Update and rename hi.js to verify.js

--- commands/hi.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message) => {
-    message.channel.send('Hello ');
-    }
-module.exports.help = {
-    name: 'hi'
-};
---
+++ commands/verify.js
@@ -0,0 +1,10 @@
+exports.run = (client, message) => {
+    var role = message.guild.roles.find(role => role.name === "Guests");
+    message.member.addRole(role);
+    message.author.send('Hello thank you for verifying! ');
+    }
+
+
+module.exports.help = {
+    name: 'verify'
+};
