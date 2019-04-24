1991da057ecaf5e6abee8e4d2df8e4ea71079f86
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-24 00:15:01 -0300

    ban.js

--- commands/pay.js
+++ commands/pay.js
@@ -1,44 +1,33 @@
 const Discord = require("discord.js");
-const fs = require("fs");
-let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-  //!pay @isatisfied 59345
 
-  if(!coins[message.author.id]){
-    return message.reply("You don't have any coins!")
-  }
+    if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send("No can do pal!");
+    if(args[0] == "help"){
+      message.reply("Usage: !ban <user> <reason>");
+      return;
+    }
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
-  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
-      coins: 0
-    };
-  }
+    let incidentchannel = message.guild.channels.find(`name`, "logs");
+    if(!incidentchannel) return message.channel.send("Can't find #logs channel.");
 
-  let pCoins = coins[pUser.id].coins;
-  let sCoins = coins[message.author.id].coins;
-
-  if(sCoins < args[0]) return message.reply("Not enough coins there!");
-
-  coins[message.author.id] = {
-    coins: sCoins - parseInt(args[1])
-  };
-
-  coins[pUser.id] = {
-    coins: pCoins + parseInt(args[1])
-  };
-
-  message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
-
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if(err) cosole.log(err)
-  });
-
-
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
 }
 
 module.exports.help = {
-  name: "pay"
+  name:"ban"
 }
