f85576002215087d515f71ed3d43b28adbe89b38
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-24 00:35:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,13 +42,12 @@ bot.on("ready", () => {
 });
 
 bot.on('guildMemberAdd', member => {
-    member.guild.channels.get("name","welcome-leave")
-    guildMemberAdd.guild.channels.send('**' + member.user.username + '**, has joined the server! there are now ' + member.guild.memberCount + ' now'); 
+  member.guild.channels.get('channelID').send('**' + member.user.username + '**, has joined the server!'); 
 });
 
 bot.on('guildMemberRemove', member => {
-    member.guild.channels.get("name","welcome-leave")
-    guildMemberRemove.guild.channels.send('**' + member.user.username + '**, has left the server! there are now ' + member.guild.memberCount + ' now');
+  member.guild.channels.get('channelID').send('**' + member.user.username + '**, has left the server');
+  //
 });
 
 
