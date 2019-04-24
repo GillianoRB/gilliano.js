bcb65fb02ee57e974fa3b3b400314c69be4a3224
GillianoRB <42073609+GillianoRB@users.noreply.github.com>
2019-04-24 14:42:54 -0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,7 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("ADMINISTRATION")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(args[0] == "help"){
+    message.reply("Usage: !addrole <user> <role>");
+    return;
+  }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user, yo.");
   let role = args.join(" ").slice(22);
@@ -17,6 +21,7 @@ module.exports.run = async (bot, message, args) => {
   try{
     await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
   }catch(e){
+    console.log(e.stack);
     message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
   }
 }
