c1aa4cb145b49f800d82b737391ee2a2d84090dc
TsunamiRaze <49965833+TsunamiRaze@users.noreply.github.com>
2019-04-25 09:15:26 +0800

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,61 +1,28 @@
 const Discord = require("discord.js");
-const fs = require("fs");
-const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-  //!warn @daeshan <reason>
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
-  let reason = args.join(" ").slice(22);
-
-  if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-  };
-
-  warns[wUser.id].warns++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-    if (err) console.log(err)
-  });
-
-  let warnEmbed = new Discord.RichEmbed()
-  .setDescription("Warns")
-  .setAuthor(message.author.username)
-  .setColor("#fc6400")
-  .addField("Warned User", `<@${wUser.id}>`)
-  .addField("Warned In", message.channel)
-  .addField("Number of Warnings", warns[wUser.id].warns)
-  .addField("Reason", reason);
-
-  let warnchannel = message.guild.channels.find(`name`, "incidents");
-  if(!warnchannel) return message.reply("Couldn't find channel");
-
-  warnchannel.send(warnEmbed);
-
-  if(warns[wUser.id].warns == 2){
-    let muterole = message.guild.roles.find(`name`, "muted");
-    if(!muterole) return message.reply("You should create that role dude.");
-
-    let mutetime = "10s";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`<@${wUser.id}> has been temporarily muted`);
-
-    setTimeout(function(){
-      wUser.removeRole(muterole.id)
-      message.reply(`<@${wUser.id}> has been unmuted.`)
-    }, ms(mutetime))
-  }
-  if(warns[wUser.id].warns == 3){
-    message.guild.member(wUser).ban(reason);
-    message.reply(`<@${wUser.id}> has been banned.`)
-  }
-
+    let user = message.mentions.members.first();
+    if (!user) return;
+    let issuer = message.author;
+    let reason = args.slice(1).join(" ") || "Please provide a reason";
+    let date = new Date();
+    let url = message.author.displayAvatarURL || message.author.defaultAvatarURL;
+    let reportChannel = message.guild.channels.find(x => x.name === 'reported-user');
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`${message.member.displayName} Reported`, message.author.avatarURL)
+        .setColor("RANDOM")
+        .addField("Reported user:", user)
+        .addField("Reported by:", issuer)
+        .addField("Reason:", reason)
+        .addField("Reported In:", message.channel)
+        .addField("Reported on:", message.createdAt)
+        .setFooter(`ID: ${user.id}`)
+        .setTimestamp([date])
+        .setThumbnail(`${user.user.displayAvatarURL}`)
+    message.channel.send(embed).then(m => m.delete(10000));
+    reportChannel.send(embed);
 }
-
 module.exports.help = {
-  name: "warn"
-}
+    name: "report",
+};
